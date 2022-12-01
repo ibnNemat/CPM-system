942eeba26dec5694a3f3a293b5cc1ffe2058aaa3
Dovud <foundation240897@gmial.com>
2022-12-01 20:41:33 +0500

    commit for changes

--- src/main/java/uz/devops/intern/redis/CustomerTelegramRedisRepository.java
+++ src/main/java/uz/devops/intern/redis/CustomerTelegramRedisRepository.java
@@ -2,7 +2,6 @@ package uz.devops.intern.redis;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
 
 @Component
 public interface CustomerTelegramRedisRepository extends CrudRepository<CustomerTelegramRedis, Long> {
